601da90e388f3291af8b3a7ca1678e2997fc96ed
Fabian Juren <fjuren@gmail.com>
2022-03-15 22:18:44 -0700

    Add building block code

---
+++ app.js
@@ -0,0 +1,26 @@
+const gameboard = (() => {
+    const board = () => [];
+    return board;
+})();
+
+const gameFlow =(() => {
+
+})
+
+
+const Players = (name) => {
+    const sayName = () => console.log(name);
+    return {sayName};
+}
+
+const Player_1 = (name) => {
+    const {sayName} = Players(name);
+    return {sayName};
+}
+
+const Player_2 = (name) => {
+    const {sayName} = Players(name);
+    return {sayName};
+}
+
+const jeff = Player_1('jeff')
