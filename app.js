c0c0142cfc4329ae780d3c833002e735bf5cbde8
Fabian Juren <fjuren@gmail.com>
2022-03-18 21:05:14 -0700

    Add ID flag on board click

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const gameboard = (() => {
-    const board = () => ['X','O','O','X','X','X','X','X'];
-    return {board};
+    const playerMoves = () => ['X','O','O','X','X','X','X','X'];
+    const boardClick = document.addEventListener('click', (e) => {
+        if (e.target.id === "") {
+            // do nothing
+        } else
+        console.log(e.target.id);
+    });
+
+    return {playerMoves};
 })();
 
 const gameFlow = (() => {
@@ -12,20 +19,22 @@ const Players = (name) => {
     return {sayName};
 }
 
-const Player_1 = (name) => {
+const Player_1 = (name, sign) => {
     const {sayName} = Players(name);
+    const _sign = sign
     return {sayName};
 }
 
-const Player_2 = (name) => {
+const Player_2 = (name, sign) => {
     const {sayName} = Players(name);
+    const _sign = sign
     return {sayName};
 }
 
 // const jeff = Player_1('jeff')
 
 const renderMove = (() => {
-    gameboard.board().forEach((move,i,a) => {
+    gameboard.playerMoves().forEach((move,i,a) => {
         document.getElementById("" + i + "").innerHTML = move;
     });
 });
