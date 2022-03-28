d0cbec03be87df9715e8df52cbbfe154c70991f0
Fabian Juren <fjuren@gmail.com>
2022-03-27 18:25:14 -0700

    Switch player turn using gameFlow fcn

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
 const X = "X"
 const O = "O"
 
-// Players added once entered by users
+// Players names & symbols are added once names are entered by users
 playerAssignments = []
 
+// form that assigns player inputted names to X & O symbols
 const form = document.getElementById("playerNames")
 // retrieve submitted names entered by users
 form.addEventListener("submit",  (e) => {
@@ -18,17 +19,16 @@ form.addEventListener("submit",  (e) => {
     // player2Name.symbol();
 
     playerAssignments.push(player1Name,player2Name)
-
     form.reset();
 });
 
-// Factory function
+// Player Factory function
 const Players = (name) => {
     const sayName = () => name;
     return {sayName};
 }
 
-// inherits from Players
+// inherits from Players factory function
 const Player_1 = (name, sign) => {
     const {sayName} = Players(name);
     const _sign = sign;
@@ -36,7 +36,7 @@ const Player_1 = (name, sign) => {
     return {sayName, symbol};
 }
 
-// inherits from Players
+// inherits from Players factory function
 const Player_2 = (name, sign) => {
     const {sayName} = Players(name);
     const _sign = sign;
@@ -44,9 +44,12 @@ const Player_2 = (name, sign) => {
     return {sayName, symbol};
 }
 
+// tracks the player moves on the tic tac toe board
 const gameboard = (() => {
     // const playerMoves = () => ['X','O','O','X','X','X','X','X'];
     const playerMoves = () => [];
+        // Prevents duplicate moves. Nothing happens if a position has already been moved.
+        // Otherwise plays the players move
     const preventDuplicateMoves = []
     const boardID = document.querySelector(".grid-container");
     const boardClick = boardID.addEventListener('click', (e) => {        
@@ -60,15 +63,24 @@ const gameboard = (() => {
             preventDuplicateMoves.push(""+ e.target.id + "");
         };
         // next step is to push the "X" to player moves, after checking whether the position was moved already
-        e.target.innerHTML = "" + playerAssignments[0].symbol() + "";
-        // console.log(playerAssignments[1].symbol())
+        e.target.innerHTML = "" + playerAssignments[turnTracker].symbol() + "";
+        gameFlow.move();
     }); 
     return {playerMoves};  
 })();
 
-const gameFlow = (() => {
 
-    // const player2Move
+// Player 1's turn if turnTracker = 0, else player 2's turn
+var turnTracker = 0
+// gameFLow controls the flow of the game
+const gameFlow = (() => {
+    const move = () => {
+        if (turnTracker === 0) {
+            turnTracker++;
+        } else {
+            turnTracker--;
+        }
+    }
 })();
 
 const renderMove = (() => {
